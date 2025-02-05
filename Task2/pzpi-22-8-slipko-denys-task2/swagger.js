07702f0fc2cb6b733ee71de000664ce52d401a14
Denys <denys.slipko@nure.ua>
2025-02-05 19:07:39 +0200

    Add files via upload

---
+++ Task2/pzpi-22-8-slipko-denys-task2/swagger.js
@@ -0,0 +1,24 @@
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Transport System API",
+            version: "1.0.0",
+            description: "API для управления общественным транспортом",
+        },
+        servers: [{ url: "http://localhost:5000/api" }],
+    },
+    apis: ["./backend/routes.js"] 
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+function setupSwagger(app) {
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    console.log("📄 Swagger доступен на http://localhost:5000/api-docs");
+}
+
+module.exports = setupSwagger;
